805b0185980522e6870712793d1e866450804fae
AbhishekRana23 <40822125+AbhishekRana23@users.noreply.github.com>
2019-11-21 17:38:25 +0530

    Delete env.sh

--- env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-export BOT_TOKEN='YOUR_TOKEN_HERE';
-export DB_ID='YOUR_ID_HERE';
-export DB_SECRET='YOUR_SECRET_HERE';
-export DB_TOKEN='YOUR_TOKEN_HERE';
-export NAVIGATION_ROOT='YOUR_PATH_HERE';
