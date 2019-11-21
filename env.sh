439769f4f3c7ee50eac52e9435c6d0428c2c2d14
AbhishekRana23 <40822125+AbhishekRana23@users.noreply.github.com>
2019-11-21 17:38:46 +0530

    Add files via upload

---
+++ env.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+export BOT_TOKEN='1043566844:AAFE0FOYjwg80oHlrCI7w6CASywK6BxspP0';
+export DB_ID='	gtoy0d3v4idnddf';
+export DB_SECRET='	zcobk7fkfcmk8va';
+export DB_TOKEN='7vA0vtw9qAAAAAAAAAAAIs70B3UQGPuwrTYvrk-LScf6fobqGkw88R4T61BtHoaP';
+export NAVIGATION_ROOT='/home/user/path/to/download/dir';
